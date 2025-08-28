406b3bfc864ba00f784865896d26a45a2deadf70
KodionHarish <168513572+KodionHarish@users.noreply.github.com>
2025-08-28 15:58:38 +0530

    presisit notify

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import notificationReducer from './notificationSlice';
+import notificationReducer from '../features/notificationSlice';
 
 // Persist config for notifications only
 const notificationPersistConfig = {
